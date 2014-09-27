d6536ac32b5c4d254b121a413e5d7460af5666b6
Pandelis Zembashis <info@pandelis.me>
2014-09-27 01:14:23 +0100

    Release 1.0.0 uploaded

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -5,10 +5,24 @@ chrome.extension.sendMessage({}, function(response) {
 
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
-		console.log($("font:contains('plus')").text());
-		
+
+	 	if ( $("font:contains('plus')").text() ){
+	 		var captcha = $("font:contains('plus')").text();
+	 	}
+	 	else if ( $("font:contains('and')").text() ){
+	 		var captcha = $("font:contains('and')").text();
+	 	}
+	 	else if ( $("font:contains('+')").text() ){
+	 		var captcha = $("font:contains('+')").text();
+	 	}
+	 	else {
+	 		console.log("Error with detecting captcha")
+	 	}
+
+	 	var number = captcha.match(/\d*/g);
+		number = $.grep(number,function(n){ return(n) });
+		var result = parseInt(number[0]) + parseInt(number[1]);
+		$("input[name='txtMathAns']").val(result);
 	}
 	}, 10);
 });
